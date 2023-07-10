438038883e75e7664cd6a3e94779d814d61d437d
Ajsen Martynov <ajsmartmin@gmail.com>
2023-07-11 00:31:52 +0900

    add calculate.js

---
+++ bin/calculate.js
@@ -0,0 +1,12 @@
+export default (operand1, operand2, operator) => {
+  switch (operator) {
+    case '+':
+      return operand1 + operand2;
+    case '-':
+      return operand1 - operand2;
+    case '*':
+      return operand1 * operand2;
+    default:
+      return null;
+  }
+};
