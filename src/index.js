e662780a312b15db7ef3e07239e0317d4c273e47
Ajsen Martynov <ajsmartmin@gmail.com>
2023-07-22 20:08:16 +0900

    refactor: rename first argument from name to getTask

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import readlineSync from 'readline-sync';
 
 const roundsCount = 3;
 
-export default (game, introduction) => {
+export default (getTask, introduction) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(introduction);
   for (let i = 0; i < roundsCount; i += 1) {
-    const [question, correctAnswer] = game();
+    const [question, correctAnswer] = getTask();
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
     if (correctAnswer !== userAnswer) {
